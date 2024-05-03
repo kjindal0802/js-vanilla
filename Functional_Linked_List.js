7e6d29b5c5ebb0fb93e96c96dc523a7fa479b6c2
kjindal0802 <kjindal0802@gmail.com>
2024-05-03 10:13:44 +0530

    Functional Linked List

--- Functional_Linked_List.js
+++ Functional_Linked_List.js
@@ -1,31 +1,31 @@
 function createLinkedList() {
-    this.head = null;
-    this.size = 0;
-
-    this.add = function(value) {
-        const node = new createNode(value);
-        if (this.head === null) {
-            this.head = node;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = node;
+  const list = {
+    head: null,
+    size: 0,
+    add: function (value) {
+      const node = new Node(value);
+      if (this.head === null) {
+        this.head = node;
+      } else {
+        let current = this.head;
+        while (current.next) {
+          current = current.next;
         }
-        this.size++;
-    };
+        current.next = node;
+      }
+      this.size++;
+    },
+    getSize: function () {
+      return this.size;
+    },
+  };
 
-    this.getSize = function() {
-        return this.size;
-    };
-
-    return { add: this.add, getSize: this.getSize };
+  return list;
 }
 
-function createNode(value, next = null) {
-    this.value = value;
-    this.next = next;
+function Node(value, next = null) {
+  this.value = value;
+  this.next = next;
 }
 
 // Usage:
@@ -33,4 +33,5 @@ const ll = createLinkedList();
 ll.add(1);
 ll.add(2);
 ll.add(3);
+console.log(ll); // Output: { head: { value: 1, next: { value: 2, next: { value: 3, next: null } } }, size: 3 }
 console.log(ll.getSize()); // Output: 3
